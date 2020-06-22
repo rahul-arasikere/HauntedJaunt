180a8d5b0db8721d0e8fb4611da13314c3d11518
Rahul Arasikere <arasikere.rahul@gmail.com>
2020-06-22 22:06:40 +0530

    caught up with class, to implement some more features

---
+++ Assets/Scripts/GameEnding.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameEnding : MonoBehaviour
+{
+    public GameObject player;
+    public CanvasGroup winPanel;
+    public CanvasGroup caughtPanel;
+    public float fadeDuration;
+    public float displayDuration;
+    private bool _isPlayerAtExit = false;
+    private bool _isPlayerCaught = false;
+    private float _timer = 0f;
+
+    private void Update()
+    {
+        if (_isPlayerAtExit)
+        {
+            EndLevel(winPanel, false);
+        }
+        else if (_isPlayerCaught)
+        {
+            EndLevel(caughtPanel, true);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            _isPlayerAtExit = true;
+        }
+    }
+
+    public void CaughtPlayer()
+    {
+        _isPlayerCaught = true;
+    }
+
+    private void EndLevel(CanvasGroup panel, bool doRestart)
+    {
+        _timer += Time.deltaTime;
+        panel.alpha = _timer / fadeDuration;
+
+        if (_timer > fadeDuration + displayDuration)
+        {
+            if (doRestart)
+            {
+                SceneManager.LoadScene(0);
+            }
+            else
+            {
+                Application.Quit();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Observer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Observer : MonoBehaviour
+{
+    public GameEnding gameEnding;
+    public Transform player;
+    private bool _isPlayerInRange;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform == player)
+        {
+            _isPlayerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.transform == player)
+        {
+            _isPlayerInRange = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (_isPlayerInRange)
+        {
+            Vector3 direction = player.position - transform.position;
+            direction.y += 0.25f;
+            Ray ray = new Ray(origin: transform.position, direction);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.transform == player)
+                {
+                    gameEnding.CaughtPlayer();
+                }
+            }
+        }
+    }
+}
