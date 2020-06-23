4c749e038f3d9e1a705f10436ed8cb8f69dabe6d
Rahul Arasikere <arasikere.rahul@gmail.com>
2020-06-23 19:42:42 +0530

    added minimap

---
+++ Assets/Scripts/MoveCamera.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour
+{
+    public Transform player;
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 position = player.position;
+        position.y = transform.position.y;
+        transform.position = position;
+    }
+}
